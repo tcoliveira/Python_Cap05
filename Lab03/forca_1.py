4bd40624686067ded93d916047ba10b6b7388ee7
tcoliveira <thiagocarlos09@gmail.com>
2020-01-31 13:42:31 -0300

    Hangman

---
+++ Lab03/forca_1.py
@@ -0,0 +1,84 @@
+import random as rd
+
+# Tabuleiro
+
+Board = ['''
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+
+# print(Board[6])
+
+class Word():
+    '''file = (open('palavras.txt', 'r'))
+    file = file.readlines()
+ #   random = rd.choice(file)'''
+    def __init__(self, file = (open('palavras.txt', 'r')).readlines()):
+        self. file = file
+        self.random = rd.choice(file)
+        print('class creation process completed')
+
+    def fPrint(self):
+        print(self.random)
+
+    def
+
+'''    def __str__():
+        return Word.randomOpen(self)
+'''
+Word().fPrint()
