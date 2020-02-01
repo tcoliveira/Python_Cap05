cb4a7e78060a55e45a8e2b4abb8a1282d5f0cfbc
tcoliveira <thiagocarlos09@gmail.com>
2020-02-01 12:48:26 -0300

    Update forca_1.py

--- Lab03/forca_1.py
+++ Lab03/forca_1.py
@@ -68,6 +68,7 @@ class Word():
     file = (open('palavras.txt', 'r'))
     file = file.readlines()
     random = rd.choice(file)
+    x = input('Digite uma letra: ')
     def __init__(self,  file = (open('palavras.txt', 'r')).readlines()):
         self. file = file
         self.random = rd.choice(file)
@@ -76,11 +77,29 @@ class Word():
     def fPrint(self):
         print(Word.random)
 
+    #Masking random word
     def Mask(self):
         print("-"*(len(Word.random)-1))
 
     '''def __str__():
         return Word.randomOpen(self)
+        
 '''
+    def wordinput(self):
+        if Word.x == Word.random:
+            print(1)
+        else:
+            print(2)
+
+class Compareword(Word):
+    x = Word.random
+    def __init__(self):
+        pass
+    def Teste():
+        print(Compareword.x)
+
+
+
 Word().fPrint()
 Word().Mask()
+Word.wordinput()
