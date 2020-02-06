e7ef1b5580eee26284b727b5779da44540974ada
tcoliveira <thiagocarlos09@gmail.com>
2020-02-06 13:02:46 -0300

    Update calculadora_classe.py

--- calculadora_classe.py
+++ calculadora_classe.py
@@ -1,40 +1,45 @@
+from math import fsum
+
 class Numero():
 	"""definindo numero"""
-	def __init__(self, x):
-		self.x = x
+	x = int(input('digite valor = '))
+	y = int(input('digite valor = '))
+	def __init__(self):
+		pass
 
 	def soma(self):
-		self.x + self.y
+		w = self.x + self.y
+		print(w)
 
 	def sub(self):
-		return self.x-self.y
+		w = self.x - self.y
+		print(w)
 
 	def mul(self):
-		return self.x*self.y
+		w = self.x * self.y
+		print(w)
 
 	def div(self):
-		return self.x/self.y
+		w = self.x / self.y
+		print(w)
 
 def main():
-	z = input('1 - Adicao\n'
+	z = int(input('1 - Adicao\n'
 		'2 - Subtracao\n'
 		'3 - Multiplicacao\n'
 		'4 - Divisao\n'
-		"Qual operacao deseja fazer? ")
-	x = input('digite valor = ')
-	y = input('digite valor = ')
-	Numero(x)
-	#while z not in range(1,5):
+		"Qual operacao deseja fazer? "))
+#while z not in range(1,5):
 	if z == 1:
-		Numero.soma()
+		Numero().soma()
 	elif z == 2:
-		Numero.sub()
+		Numero().sub()
 	elif z == 3:
-		Numero.mul()
+		Numero().mul()
 	elif z == 4:
-		Numero.div()
+		Numero().div()
 	else:
-		return 'digite outro numero'
+		print('digite outro numero')
 
 if __name__ == "__main__":
 	main()
